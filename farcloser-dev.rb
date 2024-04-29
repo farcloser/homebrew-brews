f09cb4f0048817f55afc583bbc8a719201c85597
apostasie <spam_blackhole@farcloser.world>
2024-04-29 12:13:15 -0700

    Removing transient dep

--- farcloser-dev.rb
+++ farcloser-dev.rb
@@ -6,7 +6,7 @@ class FarcloserDev < Formula
   version "dev"
 
   depends_on "farcloser/brews/mumbrew"
-  depends_on "farcloser/brews/openssh"
+  # depends_on "farcloser/brews/openssh"
   depends_on "farcloser/brews/ssh-agent"
   # Untested on linux, and homebrew is not the right way on linux anyhow
   depends_on :macos
